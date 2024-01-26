3151d168242449e3aa3eca43ea0a938f00b4a6d4
Joselia Costa <joselialcosta@gmail.com>
2024-01-26 11:46:38 +0100

    build: configuring vitest setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,11 @@ import { describe, expect, it } from "vitest";
 
 import App from "./App";
 import { render, screen } from "@testing-library/react";
-import React from "react";
 
 describe("App", () => {
   it("renders headline", () => {
-    render(<App title="React" />);
+    render(<App />);
 
-    screen.debug();
-
-    expect(screen.getByRole("heading")).toBeInTheDocument("React");
+    expect(screen.getByRole("heading")).toBeInTheDocument();
   });
 });
